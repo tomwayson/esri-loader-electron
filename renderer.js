5ea33b15679d45a1108d146916bef54135da3a8c
Tom Wayson <tom@tomwayson.com>
2019-01-14 15:08:27 -0800

    install and require esri-loader instead of using CDN via script tag

--- renderer.js
+++ renderer.js
@@ -1,9 +1,20 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+const esriLoader = require('esri-loader');
 
-// load esri modules
-esriLoader.loadModules(['esri/views/MapView', 'esri/WebMap'])
+// NOTE: have to use <= v4.7 for now due to bug
+// see: https://github.com/Esri/esri-loader/issues/147
+const url = 'https://js.arcgis.com/4.7/';
+
+// esri-loader options
+const options = { url };
+
+// load esri CSS for this version
+esriLoader.loadCss(`${url}esri/css/main.css`)
+
+// load esri JS modules
+esriLoader.loadModules(['esri/views/MapView', 'esri/WebMap'], options)
 .then(([MapView, WebMap]) => {
   // then we load a web map from an id
   var webmap = new WebMap({
